26808cddeb0d4528140cef655e898220e68d2b78
martynasapanavicius <martynas.apanavicius@gmail.com>
2026-04-03 01:02:37 +0300

    RentedArray + tests

---
+++ RCi.Toolbox.Tests/Collections/RentedArrayTests.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Runtime.InteropServices;
+using RCi.Toolbox.Collections;
+
+namespace RCi.Toolbox.Tests.Collections
+{
+    [Parallelizable(ParallelScope.All)]
+    public static class RentedArrayTests
+    {
+        private static readonly int[] _originalArray = new Func<int[]>(() =>
+        {
+            var random = new Random(42);
+            var array = new int[10];
+            for (var i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(-99, 100);
+            }
+            return array;
+        })();
+
+        private static RentedArray<int> CreateTestRentedArray()
+        {
+            var array = new RentedArray<int>(_originalArray.Length, false);
+            for (var i = 0; i < _originalArray.Length; i++)
+            {
+                array[i] = _originalArray[i];
+            }
+            return array;
+        }
+
+        [Test]
+        public static void CtorEmpty()
+        {
+            using var empty = new RentedArray<int>(0);
+            Assert.That(empty.Length, Is.EqualTo(0));
+        }
+
+        [Test]
+        public static void Dispose()
+        {
+            var actual = CreateTestRentedArray();
+            try
+            {
+                Assert.DoesNotThrow(() => actual[0] = 222);
+                Assert.That(actual[0], Is.EqualTo(222));
+            }
+            finally
+            {
+                actual.Dispose();
+            }
+            Assert.Throws<NullReferenceException>(() => actual[0] = 333);
+
+            Assert.DoesNotThrow(() => actual.Dispose());
+        }
+
+        [Test]
+        public static void Length()
+        {
+            using var actual = CreateTestRentedArray();
+            Assert.That(actual.Length, Is.EqualTo(_originalArray.Length));
+        }
+
+        [Test]
+        public static void Memory()
+        {
+            using var actual = CreateTestRentedArray();
+            Assert.That(actual.Memory.Span.SequenceEqual(_originalArray), Is.True);
+        }
+
+        [Test]
+        public static void ReadOnlyMemory()
+        {
+            using var actual = CreateTestRentedArray();
+            Assert.That(actual.ReadOnlyMemory.Span.SequenceEqual(_originalArray), Is.True);
+        }
+
+        [Test]
+        public static void Span()
+        {
+            using var actual = CreateTestRentedArray();
+            Assert.That(actual.Span.SequenceEqual(_originalArray), Is.True);
+        }
+
+        [Test]
+        public static void ReadOnlySpan()
+        {
+            using var actual = CreateTestRentedArray();
+            Assert.That(actual.ReadOnlySpan.SequenceEqual(_originalArray), Is.True);
+        }
+
+        [Test]
+        public static void GetEnumerator()
+        {
+            using var actual = CreateTestRentedArray();
+            using var enumerator = actual.GetEnumerator();
+            var c = 0;
+            while (enumerator.MoveNext())
+            {
+                Assert.That(enumerator.Current, Is.EqualTo(_originalArray[c]));
+                c++;
+            }
+            Assert.That(c, Is.EqualTo(_originalArray.Length));
+        }
+
+        [Test]
+        public static void Clear()
+        {
+            using var actual = CreateTestRentedArray();
+            Assert.That(actual.SequenceEqual(_originalArray), Is.True);
+            actual.Clear();
+            Assert.That(actual.All(x => x == 0), Is.True);
+        }
+
+        [Test]
+        public static void Contains()
+        {
+            using var actual = CreateTestRentedArray();
+            Assert.That(actual.Contains(_originalArray[0]), Is.True);
+            Assert.That(actual.Contains(int.MaxValue), Is.False);
+        }
+
+        [Test]
+        public static void CopyTo()
+        {
+            using var actual = CreateTestRentedArray();
+
+            var copy = new int[actual.Length];
+            actual.CopyTo(copy, 0);
+            Assert.That(copy.SequenceEqual(_originalArray), Is.True);
+
+            copy = new int[actual.Length + 5];
+            actual.CopyTo(copy, 2);
+            Assert.That(copy.SequenceEqual([0, 0, .. _originalArray, 0, 0, 0]), Is.True);
+        }
+
+        [Test]
+        public static void Count()
+        {
+            using var actual = CreateTestRentedArray();
+            Assert.That(actual.Count, Is.EqualTo(_originalArray.Length));
+        }
+
+        [Test]
+        public static void IsReadOnly()
+        {
+            using var actual = CreateTestRentedArray();
+            Assert.That(actual.IsReadOnly, Is.False);
+        }
+
+        [Test]
+        public static void IndexOf()
+        {
+            using var actual = CreateTestRentedArray();
+            var indexOf = actual.IndexOf(_originalArray[3]);
+            Assert.That(indexOf, Is.EqualTo(3));
+        }
+
+        [Test]
+        public static void Indexers()
+        {
+            using var actual = CreateTestRentedArray();
+            for (var i = 0; i < actual.Length; i++)
+            {
+                Assert.That(actual[i], Is.EqualTo(_originalArray[i]));
+
+                actual[i] = _originalArray[i] * -1;
+                Assert.That(actual[i], Is.EqualTo(_originalArray[i] * -1));
+            }
+        }
+
+        [Test]
+        public static void ToRentedArray_Array()
+        {
+            var original = _originalArray.ToArray();
+            using var actual = original.ToRentedArray();
+            Assert.That(actual.SequenceEqual(_originalArray));
+            for (var i = 0; i < original.Length; i++)
+            {
+                original[i] = 123;
+            }
+            Assert.That(actual.SequenceEqual(_originalArray));
+        }
+
+        [Test]
+        public static void ToRentedArray_ImmutableArray()
+        {
+            var original = _originalArray.ToImmutableArray();
+            using var actual = original.ToRentedArray();
+            Assert.That(actual.SequenceEqual(_originalArray));
+            for (var i = 0; i < original.Length; i++)
+            {
+                ImmutableCollectionsMarshal.AsArray(original)![i] = 123;
+            }
+            Assert.That(actual.SequenceEqual(_originalArray));
+        }
+
+        [Test]
+        public static void ToRentedArray_IList()
+        {
+            var original = _originalArray.ToList();
+            using var actual = original.ToRentedArray();
+            Assert.That(actual.SequenceEqual(_originalArray));
+            for (var i = 0; i < original.Count; i++)
+            {
+                original[i] = 123;
+            }
+            Assert.That(actual.SequenceEqual(_originalArray));
+        }
+
+        [Test]
+        public static void ToRentedArray_IEnumerable()
+        {
+            var original = GetStream(_originalArray);
+            using var actual = original.ToRentedArray();
+            Assert.That(actual.SequenceEqual(_originalArray));
+            return;
+
+            static IEnumerable<T> GetStream<T>(IEnumerable<T> items)
+            {
+                foreach (var item in items)
+                {
+                    yield return item;
+                }
+            }
+        }
+
+        [Test]
+        public static void UnsafeAccessUnderlyingArray()
+        {
+            using var actual = new RentedArray<int>(_originalArray.Length, false);
+            actual.UnsafeAccessUnderlyingArray(x => _originalArray.CopyTo(x, 0));
+            Assert.That(actual.SequenceEqual(_originalArray), Is.True);
+        }
+
+        [Test]
+        public static void UnsafeCreateFromExisting()
+        {
+            const int length = 1337;
+            var manuallyRentedArray = ArrayPool<int>.Shared.Rent(length);
+
+            using var actual = RentedArray.UnsafeCreateFromExisting(
+                length,
+                manuallyRentedArray,
+                ArrayPool<int>.Shared
+            );
+            Assert.That(
+                manuallyRentedArray.AsSpan()[..length].ToArray(),
+                Is.EqualTo(actual.Span.ToArray())
+            );
+            actual.UnsafeAccessUnderlyingArray(x =>
+            {
+                Assert.That(ReferenceEquals(manuallyRentedArray, x), Is.True);
+            });
+        }
+
+        [Test]
+        public static void UnsafeDetachUnderlyingArray()
+        {
+            int[]? underlying = null;
+            ArrayPool<int>? pool = null;
+            try
+            {
+                using (var actual = new RentedArray<int>(_originalArray.Length))
+                {
+                    // extract underlying array
+                    (underlying, pool) = actual.UnsafeDetachUnderlyingArray();
+
+                    // rented array no longer has underlying array reference and should throw
+                    Assert.Throws<NullReferenceException>(() => actual[0] = 1);
+                }
+
+                // all good, we extracted and now own physical array
+                underlying[0] = 1;
+            }
+            finally
+            {
+                if (underlying is not null && pool is not null)
+                {
+                    pool.Return(underlying);
+                }
+            }
+        }
+    }
+}
---
+++ RCi.Toolbox/Collections/RentedArray.cs
@@ -0,0 +1,317 @@
+﻿using System;
+using System.Buffers;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Runtime.CompilerServices;
+
+namespace RCi.Toolbox.Collections
+{
+    public static class RentedArray
+    {
+        public static RentedArray<T> UnsafeCreateFromExisting<T>(
+            int length,
+            T[] rented,
+            ArrayPool<T> pool
+        ) => RentedArray<T>.UnsafeCreateFromExisting(length, rented, pool);
+    }
+
+    public sealed class RentedArray<T> : IMemoryOwner<T>, IList<T>, IReadOnlyList<T>
+    {
+        public readonly int Length;
+        private readonly ArrayPool<T> _pool;
+        private T[] _array;
+
+        private RentedArray(int length, ArrayPool<T> pool, T[] rented)
+        {
+            Length = length;
+            _pool = pool;
+            _array = rented;
+        }
+
+        public RentedArray(int length, ArrayPool<T> pool, bool clearArray = true)
+        {
+            ArgumentOutOfRangeException.ThrowIfLessThan(length, 0);
+            _pool = pool;
+            Length = length;
+            if (length == 0)
+            {
+                _array = [];
+            }
+            else
+            {
+                _array = pool.Rent(length);
+                if (clearArray)
+                {
+                    Array.Clear(_array, 0, length);
+                }
+            }
+        }
+
+        public RentedArray(int length, bool clearArray = true)
+            : this(length, ArrayPool<T>.Shared, clearArray) { }
+
+        public RentedArray(IEnumerable<T> items, ArrayPool<T> pool)
+        {
+            _pool = pool;
+            switch (items)
+            {
+                case T[] array:
+                    Length = array.Length;
+                    if (array.Length == 0)
+                    {
+                        _array = [];
+                    }
+                    else
+                    {
+                        _array = pool.Rent(Length);
+                        array.CopyTo(_array.AsSpan());
+                    }
+                    break;
+
+                case ImmutableArray<T> immutableArray:
+                    Length = immutableArray.Length;
+                    if (immutableArray.Length == 0)
+                    {
+                        _array = [];
+                    }
+                    else
+                    {
+                        _array = pool.Rent(Length);
+                        immutableArray.CopyTo(_array.AsSpan());
+                    }
+                    break;
+
+                case ICollection<T> collection:
+                    {
+                        var length = collection.Count;
+                        Length = length;
+                        if (length == 0)
+                        {
+                            _array = [];
+                        }
+                        else
+                        {
+                            _array = pool.Rent(Length);
+                            collection.CopyTo(_array, 0);
+                        }
+                    }
+                    break;
+
+                default:
+                    {
+                        using var rentedList = items.ToRentedList();
+                        var length = rentedList.Count;
+                        Length = length;
+                        if (length == 0)
+                        {
+                            _array = [];
+                        }
+                        else
+                        {
+                            _array = pool.Rent(Length);
+                            rentedList.CopyTo(_array, 0);
+                        }
+                    }
+                    break;
+            }
+        }
+
+        public RentedArray(IEnumerable<T> items)
+            : this(items, ArrayPool<T>.Shared) { }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        ~RentedArray() => Dispose(false);
+
+        private void Dispose(bool disposing)
+        {
+            if (_array is null)
+            {
+                return;
+            }
+            if (disposing && Length != 0)
+            {
+                var needsClearing = RuntimeHelpers.IsReferenceOrContainsReferences<T>();
+                _pool.Return(_array, needsClearing);
+            }
+            _array = null!;
+        }
+
+        public Memory<T> Memory => new(_array, 0, Length);
+
+        public ReadOnlyMemory<T> ReadOnlyMemory => new(_array, 0, Length);
+
+        public Span<T> Span => new(_array, 0, Length);
+
+        public ReadOnlySpan<T> ReadOnlySpan => new(_array, 0, Length);
+
+        #region // IEnumerable<T>
+
+        public struct Enumerator : IEnumerator<T>
+        {
+            private readonly RentedArray<T> _array;
+            private nint _index;
+
+            internal Enumerator(RentedArray<T> array)
+            {
+                _array = array;
+                _index = -1;
+            }
+
+            public void Dispose() { }
+
+            public bool MoveNext()
+            {
+                var index = _index + 1;
+                var length = _array.Length;
+                if ((nuint)index >= (nuint)length)
+                {
+                    _index = length;
+                    return false;
+                }
+                _index = index;
+                return true;
+            }
+
+            public T Current
+            {
+                get
+                {
+                    var index = _index;
+                    var array = _array;
+                    if ((nuint)index < (nuint)array.Length)
+                    {
+                        return array._array[index];
+                    }
+                    if (index < 0)
+                    {
+                        throw new InvalidOperationException("enumeration not started");
+                    }
+                    throw new InvalidOperationException("enumeration ended");
+                }
+            }
+
+            object? IEnumerator.Current => Current;
+
+            public void Reset() => _index = -1;
+        }
+
+        public Enumerator GetEnumerator() => new(this);
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator() => new Enumerator(this);
+
+        IEnumerator IEnumerable.GetEnumerator() => new Enumerator(this);
+
+        #endregion
+
+        #region // ICollection<T>
+
+        public void Add(T item) => throw new InvalidOperationException("cannot add items to array");
+
+        public void Clear()
+        {
+            var length = Length;
+            if (length > 0)
+            {
+                Array.Clear(_array, 0, length);
+            }
+        }
+
+        public bool Contains(T item) => Length != 0 && IndexOf(item) >= 0;
+
+        public void CopyTo(T[] array, int arrayIndex) =>
+            Array.Copy(_array, 0, array, arrayIndex, Length);
+
+        public bool Remove(T item) =>
+            throw new InvalidOperationException("cannot remove items from array");
+
+        public int Count => Length;
+
+        public bool IsReadOnly => false;
+
+        #endregion
+
+        #region // IList<T>
+
+        public int IndexOf(T item) => Array.IndexOf(_array, item, 0, Length);
+
+        public void Insert(int index, T item) =>
+            throw new InvalidOperationException("cannot insert items to array");
+
+        public void RemoveAt(int index) =>
+            throw new InvalidOperationException("cannot remove items from array");
+
+        public T this[int index]
+        {
+            get
+            {
+                if ((uint)index >= (uint)Length)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(index));
+                }
+                return _array[index];
+            }
+            set
+            {
+                if ((uint)index >= (uint)Length)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(index));
+                }
+                _array[index] = value;
+            }
+        }
+
+        #endregion
+
+        /// <summary>
+        /// WARNING: DO NOT STORE EXPOSED UNDERLYING ARRAY REFERENCE!
+        /// If the reference of the exposed underlying array will be stored somewhere,
+        /// the <see cref="RentedArray{T}"/> will not know about this and will return rented array it to the pool.
+        /// Note that underlying array's length might be larger.
+        /// </summary>
+        public void UnsafeAccessUnderlyingArray(Action<T[]> action) => action(_array);
+
+        /// <summary>
+        /// WARNING: use with caution!
+        /// Creates <see cref="RentedArray{T}"/> using provided rented array and will start managing it
+        /// (will return to provided <see cref="pool"/> on <see cref="Dispose"/>).
+        /// </summary>
+        public static RentedArray<T> UnsafeCreateFromExisting(
+            int length,
+            T[] rented,
+            ArrayPool<T> pool
+        )
+        {
+            ArgumentOutOfRangeException.ThrowIfGreaterThan(length, rented.Length);
+            return new RentedArray<T>(length, pool, rented);
+        }
+
+        /// <summary>
+        /// WARNING: use with caution!
+        /// This detaches rented array from the container (<see cref="RentedArray{T}"/>) and
+        /// won't be returned to <see cref="ArrayPool{T}"/> on <see cref="Dispose"/>.
+        /// User is now responsible to return the array to the pool.
+        /// </summary>
+        public (T[] Array, ArrayPool<T> Pool) UnsafeDetachUnderlyingArray()
+        {
+            var array = _array;
+            _array = null!;
+            return (array, _pool);
+        }
+    }
+
+    public static class RentedArrayExtensions
+    {
+        extension<T>(IEnumerable<T> items)
+        {
+            public RentedArray<T> ToRentedArray(ArrayPool<T> pool) => new(items, pool);
+
+            public RentedArray<T> ToRentedArray() => items.ToRentedArray(ArrayPool<T>.Shared);
+        }
+    }
+}
